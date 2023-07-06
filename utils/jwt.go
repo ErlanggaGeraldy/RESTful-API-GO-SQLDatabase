8fea3d1ad8a59a455b8e651162418a737d4164cc
Erlangga Geraldy <79989916+ErlanggaGeraldy@users.noreply.github.com>
2023-07-06 23:55:07 +0700

    Add files via upload

---
+++ utils/jwt.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var SecretKey = "SECRET_TOKEN"
+
+func GenerateToken(claims *jwt.MapClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	webtoken, err := token.SignedString([]byte(SecretKey))
+	if err != nil {
+		return "", err
+	}
+	return webtoken, nil
+}
+
+func VerifyToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, IsValid := token.Method.(*jwt.SigningMethodHMAC); !IsValid {
+			return nil, fmt.Errorf("unexpected signing method: v%", token.Header["alg"])
+		}
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
